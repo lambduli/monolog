3b941dc218bfbae0abf1a6c313368b7dc7a48720
lambduli <lambduli@gmail.com>
2022-05-14 14:51:27 +0200

    add AST Negation for not-provable

---
+++ src/ast/negation.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'set'
+
+require_relative './ast'
+
+# _, _
+class Negation < AST
+  attr_accessor :arg
+
+  def initialize(arg)
+    @arg = arg
+  end
+
+  def to_s
+    "\+ #{@arg}"
+  end
+
+  def vars
+    @arg.vars
+  end
+
+  def rename(mapping)
+    renamed = @arg.rename(mapping)
+    Negation.new(renamed)
+  end
+
+  def dup
+    Negation.new(@arg.dup)
+  end
+
+  def ==(other)
+    other.instance_of?(Negation) && @arg == other.arg
+  end
+end
